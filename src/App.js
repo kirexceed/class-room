f738b36bac925714397c3200339fed675f4079ce
NikitaVysochin <nikitavysochin.exceedteam@gmail.com>
2022-03-02 17:25:44 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
+import axios from "axios"
+import './App.css'
 
 function App() {
+  const [tables, setTables] = useState([])
+  const [users, setUsers] = useState([])
+
+  const userByTables = tables.map((table)=> {
+    return {
+      table,
+      users: users.filter(user => user.object_id === table.id)
+    }
+  })
+    
+  useEffect(async () => {
+    Promise.all([])
+
+    await axios
+      .get("http://goodsok.ru/mock-api/objects.php")
+      .then((res) => {
+        setTables(res.data)
+      });
+    await axios
+    .get("http://goodsok.ru/mock-api/users.php")
+    .then((res) => {
+      setUsers(res.data)
+    });
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mainContainer'>
+      {userByTables.map(({table, users}) => {
+        const divStyle = {
+          position: 'absolute',
+          left: table.left,
+          top: table.top,
+          transform: `rotate(${table.angle}deg)`,
+          border: '1px solid grey'
+        }
+
+        return <div key={table.id} className={table.type} style={divStyle}>
+          {users.map(user => <img key={user.id} src={user.avatar} />)}
+        </div>
+      })}
     </div>
   );
 }
